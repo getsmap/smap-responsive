9e6ec80cbd7fdf94293a1f28649b5a0fa7dda8dc
Johan Lahti <johanlahti@gmail.com>
2014-01-14 14:40:54 +0100

    #12 plugin template

---
+++ plugins/PluginTemplate.js
@@ -0,0 +1,50 @@
+L.Control.Template = L.Control.extend({
+	options: {
+		position: 'bottomright',
+		prefix: '<a href="http://leafletjs.com" title="A JS library for interactive maps">Leaflet</a>'
+	},
+
+	initialize: function(options) {
+		L.setOptions(this, options);
+	},
+
+	onAdd: function(map) {
+		this.map = map;
+		
+		this._container = L.DomUtil.create('div', 'leaflet-control-template'); // second parameter is class name
+		L.DomEvent.disableClickPropagation(this._container);
+		
+		// Use $ prefix for all jQuery objects to make it easier to sort out all
+		// jQuery dependencies when sharing the code in future.
+		this.$container = $(this._container);
+		
+		// Binding an event (example)
+		// this.map.on('layeradd', this._onLayerAdd, this).on('layerremove', this._onLayerRemove, this);
+
+		return this._container;
+	},
+
+	onRemove: function(map) {
+		// Do everything "opposite" of onAdd Ð e.g. unbind events and destroy things
+		// map.off('layeradd', this._onLayerAdd).off('layerremove', this._onLayerRemove);
+	}
+});
+
+
+// Do something when the map initializes (example taken from Leaflet attribution control)
+
+//L.Map.addInitHook(function () {
+//	if (this.options.attributionControl) {
+//		this.attributionControl = (new L.Control.Template()).addTo(this);
+//	}
+//});
+
+
+/*
+ * This code just makes removes the need for
+ * using "new" when instantiating the class. It
+ * is a Leaflet convention and should be there.
+ */
+L.control.template = function (options) {
+	return new L.Control.Template(options);
+};
