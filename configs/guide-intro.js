b608c18983f6ef6e372249c6c2ebb4a369e7d8bb
Johan Lahti <johanlahti@gmail.com>
2014-01-28 12:56:11 +0100

    config file

--- configs/guide-intro.js
+++ configs/guide-intro.js
@@ -15,28 +15,28 @@ var config = {
 		ol: [],
 			
 		bl: [
-  	 	{
-			init: "L.TileLayer",
-			url: 'http://xyz.malmo.se/data_e/Tilecache/malmo/malmo_leaflet_cache_EPSG900913/{z}/{x}/{y}.jpeg',
-			options: {
-				layerId: "malmotile",
-				displayName: "Malmö karta",
-				attribution: "© Malmö Stadsbyggnadskontor",
-				minZoom: 6,
-				maxZoom: 18,
-				tms: true
-			}
-		}
-//		    {
-//				init: "L.TileLayer",
-//				url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-//				options: {
-//					layerId: "osm",
-//					displayName: "OSM",
-//					attribution: '<span>© OpenStreetMap contributors</span>&nbsp;|&nbsp;<span>Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png"></span>',
-//					maxZoom: 18
-//				}
+//  	 	{
+//			init: "L.TileLayer",
+//			url: 'http://xyz.malmo.se/data_e/Tilecache/malmo/malmo_leaflet_cache_EPSG900913/{z}/{x}/{y}.jpeg',
+//			options: {
+//				layerId: "malmotile",
+//				displayName: "Malmö karta",
+//				attribution: "© Malmö Stadsbyggnadskontor",
+//				minZoom: 6,
+//				maxZoom: 18,
+//				tms: true
 //			}
+//		}
+		    {
+				init: "L.TileLayer",
+				url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+				options: {
+					layerId: "osm",
+					displayName: "OSM",
+					attribution: '<span>© OpenStreetMap contributors</span>&nbsp;|&nbsp;<span>Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png"></span>',
+					maxZoom: 18
+				}
+			}
 		],
 		
 		plugins: [
