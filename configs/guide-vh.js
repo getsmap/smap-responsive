602fb5e5e8b8c73c25a5bc1d4021f97caec76e5e
Johan Lahti <johanlahti@gmail.com>
2014-02-04 14:53:07 +0100

    config file related to #14

--- configs/guide-vh.js
+++ configs/guide-vh.js
@@ -118,6 +118,12 @@ var config = {
 		        		   buffer: 5
 		        	   }
 		           },
+		           {
+		        	   init: "L.Control.Geolocate",
+		        	   options: {
+		        		   
+		        	   }
+		           },
 //		           {
 //		        	   init: "L.Control.GuideIntroScreen",
 //		        	   options: {
