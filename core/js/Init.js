99e3537b049a5bf16643cb07f449a25fa6eaa7ed
johan-lahti <info@lahtitude.se>
2014-03-11 16:17:04 +0100

    #28

--- core/js/Init.js
+++ core/js/Init.js
@@ -183,7 +183,7 @@ $(document).ready(function() {
 		  if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
 		    var msViewportStyle = document.createElement("style");
 		    msViewportStyle.appendChild(
-		      document.createTextNode("@-ms-viewport{width:auto!important}")
+		      document.createTextNode("@-ms-viewport{width:100%!important}")
 		    );
 		    document.getElementsByTagName("head")[0].appendChild(msViewportStyle);
 		  }
