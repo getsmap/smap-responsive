168b4d2be5481bb0d5fe65d0138b4a1494ded370
johan-lahti <info@lahtitude.se>
2014-03-11 10:12:30 +0100

    #25 Toggle button listens to click event also

--- plugins/LayerSwitcher/LayerSwitcher.js
+++ plugins/LayerSwitcher/LayerSwitcher.js
@@ -144,7 +144,7 @@ L.Control.LayerSwitcher = L.Control.extend({
 	_addBtn: function() {
 		var btn = $('<div id="lswitch-btn"><img src="img/glyphicons_113_wide.png"></img></div>');
 		$("#mapdiv").prepend(btn);
-		btn.on("touchstart mousedown", $.proxy(function() {
+		btn.on("click touchstart mousedown", $.proxy(function() {
 			this.showPanel();
 			return false;
 		}, this));
