bbd336406f0742810e0330c6cb780883b81f105b
johanlahti <info@lahtitude.se>
2014-05-07 13:10:15 +0200

    #59 testing with animate false for setView

--- plugins/Search/Search.js
+++ plugins/Search/Search.js
@@ -254,10 +254,10 @@ L.Control.Search = L.Control.extend({
 				this.marker = L.marker(latLng).addTo(this.map);
 				this.marker.options.q = q; // Store for creating link to map
 				
-				
 				this.marker.bindPopup('<p class="lead">'+q+'</p><div><button id="smap-search-popupbtn" class="btn btn-default">'+this.lang.remove+'</button></div>');
+				
+				this.map.setView(latLng, 15, {animate: false}); // animate false fixes bug for IE10 where map turns white: https://github.com/getsmap/smap-mobile/issues/59
 				this.marker.openPopup();
-				this.map.setView(latLng, 15);
 				$("#smap-search-div input").val(null);
 				$("#smap-search-div input").blur();
 				setTimeout(function() {
