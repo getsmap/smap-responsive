7ff467a25197504c9a628db872e3e2cfe556b6dc
johanlahti <info@lahtitude.se>
2014-06-17 13:49:48 +0200

    #71

--- core/js/Init.js
+++ core/js/Init.js
@@ -32,7 +32,7 @@ smap.core.Init = L.Class.extend({
 				smap.config.langCode = lang ? lang.split("-")[0] : "en";
 				self.applyConfig(smap.config);
 				
-				params = $.extend(smap.config.params || {}, params;
+				params = $.extend(smap.config.params || {}, params);
 				smap.core.paramInst.applyParams(params);
 				smap.cmd.loading(false);
 		}).fail(function(a, text, c) {
