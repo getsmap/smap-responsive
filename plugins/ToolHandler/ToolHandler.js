ab847dc2b2db5b4ab22e6ff52dd9b7aaeb2cc6aa
johanlahti <info@lahtitude.se>
2014-08-04 13:51:01 +0200

    #87

--- plugins/ToolHandler/ToolHandler.js
+++ plugins/ToolHandler/ToolHandler.js
@@ -1,7 +1,6 @@
 L.Control.ToolHandler = L.Control.extend({
     options: {
-        position: 'bottomright',
-        addToMenu: false
+        position: 'bottomright'
     },
 
     _lang: {
@@ -23,18 +22,21 @@ L.Control.ToolHandler = L.Control.extend({
 
     onAdd: function(map) {
         var self = this;
-        smap.event.on("smap.core.pluginsadded", function() {
-            $('.leaflet-control').children("button").each(function(){
-               $(this).addClass("th-btn");
-               self.$thDiv.append($(this));
-            });
-        });
         self._container = L.DomUtil.create('div', 'leaflet-control-toolhandler'); // second parameter is class name
         L.DomEvent.disableClickPropagation(self._container);
 
         self.$container = $(self._container);
         self.$container.css("display", "none");
-        self._makeToolHandler();
+
+        this._makeToolHandler();
+        smap.event.on("smap.core.pluginsadded", function() {
+            $('.leaflet-control').children("button").each(function(){
+               // $(this).addClass("th-btn");
+               self._addButton( $(this) );
+            });
+        });
+
+        // smap.event.on("smap.core.pluginsadded", $.proxy(, this));
 
         return self._container;
     },
@@ -43,27 +45,96 @@ L.Control.ToolHandler = L.Control.extend({
 
     _makeToolHandler: function() {
         var self = this;
-        var $thDiv = $('<div id="toolhandler-container" class="toolhandler-div btn-group-lg"></div>');
-        self.$thDiv = $thDiv;
 
-        var thBtn = $('<div id="toolhandler-btn"><button id="thbtn" type="button" class="btn btn-default"><span class="fa fa-th"></span></button></div>');
-        $(thBtn).children("button").click(function(){
-            if( $("#toolhandler-container").hasClass("toolhandler-div-small") ){
-                $("#toolhandler-container").removeClass("toolhandler-div-small btn-group-vertical").addClass("toolhandler-div btn-group-lg");
+        var $thCont = $(".leaflet-top.leaflet-right")  //$('<div class="thandler-container" class="thandler-div btn-group-lg"></div>');
+        this.$thCont = $thCont;
+
+        $thCont.addClass("thandler-container");
+
+        // The button that toggles the tool container
+        var $thBtn = $('<div class="thandler-btn popover-dismiss"><button type="button" class="btn btn-default thbtn"><span class="fa fa-th"></span></button></div>');
+
+        $("#mapdiv").append($thBtn);
+        $thBtn.on("click", function() {
+            var $this = $(this);
+            if ( !$this.data('bs.popover') ) {
+                $this.popover({
+                    // container: 'body',
+                    content: null,
+                    placement: "bottom"
+                    , title: "Tools"
+                    , trigger: "manual"
+                });
+
+                $this.on("focusout", function() {
+                    $this.popover("hide");
+                    console.log("focusout");
+                });
+
+                $this.on("shown.bs.popover", function() {
+                    var $popover = $(".popover"),
+                        $popCont = $(".popover-content");
+                    // $popover.find("h3").remove();
+                    $popover.addClass("thandler-popover");
+                    $popCont.append( $(".thandler-container").children() );
+                });
+                $this.on("hidden.bs.popover", function() {
+                    var $popCont = $(".popover-content");
+
+                    // It seems as though this event is triggered before animation 
+                    // is completed - so we need to add a timeout, to avoid ugly animation.
+                    setTimeout(function() {
+                        $(".thandler-container").append( $popCont.children() );
+                    }, 150);
+                });
+                // $(".thandler-container").toggleClass("thandler-visible");
             }
-            else{
-                $("#toolhandler-container").removeClass("toolhandler-div btn-group-lg").addClass("toolhandler-div-small btn-group-vertical");
-            }
+            
+            $this.popover("toggle");
+            return false;
         });
 
-        $(window).resize(function(){    
-            if ( $("#toolhandler-btn").css("display") == "none"){
-                $("#toolhandler-container").addClass("toolhandler-div btn-group-lg").removeClass("toolhandler-div-small btn-group-vertical");
-            }
+        // $(window).on("resize", function() {
+        //     $(".thandler-btn").popover("hide");
+        // });
+        $thBtn.on("dblclick", function() {
+            return false;
         });
-        $("#mapdiv").append(thBtn);
-        $("#mapdiv").append($thDiv);
+
+        // self.$thCont.append($thBtn).append($thCont);
+        // $("#mapdiv").append( self.$thCont);
+
+        // TODO: Should be possible to solve purely with CSS
+        // $(window).resize(function(){    
+        //     if ( $(".thandler-btn").css("display") == "none"){
+        //         $(".thandler-container").addClass("thandler-div btn-group-lg").removeClass("thandler-div-small btn-group-vertical");
+        //     }
+        // });
     },
+
+    /**
+     * Each time a button is added this function should be called
+     * @param {[type]} btn [description]
+     */
+    _addButton: function($btn) {
+        // this.$thCont.append( $btn.parent() );
+
+
+        // $btn.children("button").click(function(){
+        //     if ($(".thandler-container").hasClass("thandler-div-small") ){
+        //         $(".thandler-container").removeClass("thandler-div-small btn-group-vertical").addClass("thandler-div btn-group-lg");
+        //     }
+        //     else {
+        //         $(".thandler-container").removeClass("thandler-div btn-group-lg").addClass("thandler-div-small btn-group-vertical");
+        //     }
+        // });
+    },
+
+    /**
+     * Each time a button is remove this function should be called
+     * @param {[type]} btn [description]
+     */
+    _removeButton: function(btn) {},
 
     onRemove: function(map) {
         // Do everything "opposite" of onAdd – e.g. unbind events and destroy things
