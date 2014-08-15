171dac7495dfbdee19dd223ea25571a26aa51331
tyskan <tyskan@gmail.com>
2014-08-15 12:17:37 +0200

    search test
    search test

---
+++ plugins/OpenSearch/OpenSearch.js
@@ -0,0 +1,51 @@
+
+L.Control.Template = L.Control.extend({
+	options: {
+		position: 'bottomright'
+	},
+	
+	_lang: {
+		"sv": {
+			exampleLabel: "Ett exempel"
+		},
+		"en": {
+			exampleLabel: "An example"
+		}
+	},
+	
+	_setLang: function(langCode) {
+		langCode = langCode || smap.config.langCode;
+		if (this._lang) {
+			this.lang = this._lang ? this._lang[langCode] : null;			
+		}
+	},
+
+	initialize: function(options) {
+		L.setOptions(this, options);
+		this._setLang(options.langCode);
+	},
+
+	onAdd: function(map) {
+		this.map = map;
+		
+		this._container = L.DomUtil.create('div', 'leaflet-control-template'); // second parameter is class name
+		L.DomEvent.disableClickPropagation(this._container);
+		
+		this.$container = $(this._container);
+		
+		return this._container;
+	},
+
+	onRemove: function(map) {
+		// Do everything "opposite" of onAdd – e.g. unbind events and destroy things
+		// map.off('layeradd', this._onLayerAdd).off('layerremove', this._onLayerRemove);
+	}
+});
+
+/*
+ * This code lets us skip "new" before the
+ * Class name when instantiating it.
+ */
+//L.control.template = function (options) {
+//	return new L.Control.Template(options);
+//};
