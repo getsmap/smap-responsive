4a419aa9b520ceb7f4865b4732962bd718ead9b5
Cleber <cleber.domingosarruda@helsingborg.se>
2014-11-11 13:44:38 +0100

    #106

--- dist/lib/leaflet.draw/dist/leaflet.draw-src.js
+++ dist/lib/leaflet.draw/dist/leaflet.draw-src.js
@@ -1,6 +1,6 @@
 /*
 	Leaflet.draw, a plugin that adds drawing and editing tools to Leaflet powered maps.
-	(c) 2012-2013, Jacob Toye, Smartrak
+	(c) 2012-2013, Jacob Toye, Smartrak.
 
 	https://github.com/Leaflet/Leaflet.draw
 	http://leafletjs.com
