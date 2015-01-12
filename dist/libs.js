b3c95f72c65d494b1a8f3f54aa7227b154efe418
johanlahti <info@lahtitude.se>
2015-01-12 14:06:58 +0100

    IE bugs

--- dist/libs.js
+++ dist/libs.js
@@ -32708,6 +32708,56 @@ EsriLeaflet.Controls.logo = function(options){
 	}
 })();
 
+/*
+ * Extends L.Polyline to retrieve measured distance.
+ *
+ * https://github.com/danimt/Leaflet.PolylineMeasuredDistance
+ */
+
+L.Polyline.prototype.measuredDistance = function (options) {
+  // Default options
+  this.defaults = {
+    metric: true
+  };
+  // Extend options
+  this.options = L.extend(this.defaults, options);
+
+  var distance = 0,  coords = null, coordsArray = this._latlngs;
+
+  for (i = 0; i < coordsArray.length - 1; i++) {
+    coords = coordsArray[i];
+    distance += coords.distanceTo(coordsArray[i + 1]);
+  }
+
+  // Return formatted distance
+  return L.PolylineUtil.readableDistance(distance, this.options.metric);
+};
+
+L.PolylineUtil = {
+  readableDistance: function (distance, isMetric) {
+    var distanceStr;
+
+    if (isMetric) {
+      // show metres when distance is < 1km, then show km
+      if (distance > 1000) {
+        distanceStr = (distance  / 1000).toFixed(2) + ' km';
+      } else {
+        distanceStr = Math.ceil(distance) + ' m';
+      }
+    } else {
+      distance *= 1.09361;
+
+      if (distance > 1760) {
+        distanceStr = (distance / 1760).toFixed(2) + ' miles';
+      } else {
+        distanceStr = Math.ceil(distance) + ' yd';
+      }
+    }
+
+    return distanceStr;
+  }
+};
+
 /**
  * Leaflet.UserMarker v1.0
  * 
@@ -32812,56 +32862,6 @@ EsriLeaflet.Controls.logo = function(options){
         return new L.UserMarker(latlng, options);
     };
 })(window);
-
-/*
- * Extends L.Polyline to retrieve measured distance.
- *
- * https://github.com/danimt/Leaflet.PolylineMeasuredDistance
- */
-
-L.Polyline.prototype.measuredDistance = function (options) {
-  // Default options
-  this.defaults = {
-    metric: true
-  };
-  // Extend options
-  this.options = L.extend(this.defaults, options);
-
-  var distance = 0,  coords = null, coordsArray = this._latlngs;
-
-  for (i = 0; i < coordsArray.length - 1; i++) {
-    coords = coordsArray[i];
-    distance += coords.distanceTo(coordsArray[i + 1]);
-  }
-
-  // Return formatted distance
-  return L.PolylineUtil.readableDistance(distance, this.options.metric);
-};
-
-L.PolylineUtil = {
-  readableDistance: function (distance, isMetric) {
-    var distanceStr;
-
-    if (isMetric) {
-      // show metres when distance is < 1km, then show km
-      if (distance > 1000) {
-        distanceStr = (distance  / 1000).toFixed(2) + ' km';
-      } else {
-        distanceStr = Math.ceil(distance) + ' m';
-      }
-    } else {
-      distance *= 1.09361;
-
-      if (distance > 1760) {
-        distanceStr = (distance / 1760).toFixed(2) + ' miles';
-      } else {
-        distanceStr = Math.ceil(distance) + ' yd';
-      }
-    }
-
-    return distanceStr;
-  }
-};
 
 L.Path.include(
     {
