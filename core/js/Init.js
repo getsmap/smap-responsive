d84218a9dfeafc799be19446effb0ebe98d2ef60
johanlahti <info@lahtitude.se>
2015-02-03 13:54:02 +0100

    legend auto-create add version param

--- core/js/Init.js
+++ core/js/Init.js
@@ -204,7 +204,7 @@ smap.core.Init = L.Class.extend({
 		if (wmsLayers.split(",").length > 1) {
 			wmsLayers = wmsLayers.split(",")[0]; // Use first if many layers
 		}
-		var paramsString = "request=GetLegendGraphic&format=image/png&width=20&height=20&layer=" + wmsLayers;
+		var paramsString = "version=1.1.1&request=GetLegendGraphic&format=image/png&width=20&height=20&layer=" + wmsLayers;
 		return utils.urlAppend(url, paramsString);
 	},
 	
