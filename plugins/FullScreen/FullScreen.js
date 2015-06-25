5e861b5f1f1802bb38893cfd007b855ace4ae232
peterahlstrom <ahlstrom.peter@gmail.com>
2015-06-25 08:38:21 +0200

    #180
    added options

--- plugins/FullScreen/FullScreen.js
+++ plugins/FullScreen/FullScreen.js
@@ -1,17 +1,74 @@
 
 L.Control.FullScreen = L.Control.extend({
 	options: {
-		position: 'topright'
+		position: 'topright',
+		url_root: location.origin + location.pathname + '?',
+
+		// The type of fullscreen to display: replace and newTab is most suitable for maps in an iframe. full is 'real' HTML5-fullscreen. 
+		// replaceWindow: open map in the same window, like a regular html link. newTab: open map in new tab. realFull: show map in fullscreen.
+		mode: 'full' // 'replace', 'newTab', or 'full'
+		
+	},
+
+	_properties: function() {
+		var self = this;
+		var l = this.lang;
+		switch (this.options.mode) {
+			case 'replace':
+				return {
+					expand: 'fa fa-expand',
+					compress: 'fa fa-compress',
+					expTitle: l.viewLarge,
+					compTitle: l.back,
+					action: function () {
+						window.parent.location.href = smap.cmd.createParams(self.options.url_root);
+					}
+				}
+				break;
+
+			case 'newTab':
+				return {
+					expand: 'fa fa-external-link',
+					compress: 'fa fa-compress',
+					expTitle: l.newTab,
+					compTitle: l.back,
+					action: function () {
+						window.open(smap.cmd.createParams(self.options.url_root));
+					}
+				}
+				break;
+
+			case 'full':
+				return {
+					expand: 'fa fa-expand',
+					compress: 'fa fa-compress',
+					expTitle: l.fullExp,
+					compTitle: l.reset,
+					action: function () {
+						var elem = document.getElementById('body');
+						if (screenfull.enabled) {
+							screenfull.toggle(elem);
+						}
+					}
+				}
+				break;
+		}
 	},
 	
 	_lang: {
 		"sv": {
-			btnExpand: "Visa i fullskärm",
-			btnCompress: "Återställ"
+			viewLarge: 'Visa större karta',
+			newTab: 'Öppna kartan i ny flik',
+			fullExp: "Visa kartan i fullskärm",
+			reset: "Återställ",
+			back: 'Tillbaka'
 		},
 		"en": {
-			btnExpand: "Show in fullscreen",
-			btnCompress: "Reset"
+			viewLarge: 'Show larger map',
+			newTab: 'Open map in new tab',
+			fullExp: "Show in fullscreen",
+			reset: "Reset",
+			back: 'Go back'
 		}
 	},
 	
@@ -48,27 +105,25 @@ L.Control.FullScreen = L.Control.extend({
 	},
 
 	activate: function() {
-		var elem = document.getElementById('body');
 
-		if (screenfull.enabled) {
-			screenfull.toggle(elem);
-		}
+		this._properties().action();
 	},
 
 	_updateButton: function() {
-		var self = this;
-
-		var $btn = $('#smap-fullscreen-btn');
-		//var $label = $('#smap-fullscreen-btn').find('title');
+		var self = this,
+			$btn = $('#smap-fullscreen-btn'),
+			p = this._properties()
 
 		if (screenfull.isFullscreen) {			
-			$btn.find('span').addClass('fa-compress');
-			$btn.attr('title', self.lang.btnCompress);
+			$btn.find('span').removeClass(p.expand).addClass(p.compress); // change button
+			$btn.attr('data-original-title', p.compTitle); // change toltip text
 		}
 		else {			
-			$btn.find('span').removeClass('fa-compress');
-			$btn.attr('title', self.lang.btnExpand);
+			$btn.find('span').removeClass(p.compress).addClass(p.expand);
+			$btn.attr('data-original-title', p.expTitle);
+
 		}
+		$btn.blur();
 
 	},
 
@@ -80,7 +135,7 @@ L.Control.FullScreen = L.Control.extend({
 	_createBtn: function() {
 		var self = this;
 
-		this.$btn = $('<button id="smap-fullscreen-btn" title="' + self.lang.btnExpand + '" class="btn btn-default"><span class="fa fa-expand"></span></button>');
+		this.$btn = $('<button id="smap-fullscreen-btn" title="' + self._properties().expTitle + '" class="btn btn-default"><span class="' + self._properties().expand + '"></span></button>');
 		this.$btn.on("click", function () {
 			self.activate();
 			return false;
