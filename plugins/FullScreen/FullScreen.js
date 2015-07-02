05de30b7b04ae9c652486d88c6c7882a96dcf340
peterahlstrom <ahlstrom.peter@gmail.com>
2015-07-02 09:30:54 +0200

    #180
    Doesn’t display button if fullscreen’s not allowed/supported

--- plugins/FullScreen/FullScreen.js
+++ plugins/FullScreen/FullScreen.js
@@ -1,4 +1,3 @@
-
 L.Control.FullScreen = L.Control.extend({
 	options: {
 		position: 'topright',
@@ -45,7 +44,6 @@ L.Control.FullScreen = L.Control.extend({
 					expTitle: l.fullExp,
 					compTitle: l.reset,
 					action: function () {
-						self._enableFullscreen();
 						var elem = document.getElementById('body');
 						if (screenfull.enabled) {
 							screenfull.toggle(elem);
@@ -62,14 +60,16 @@ L.Control.FullScreen = L.Control.extend({
 			newTab: 'Öppna kartan i ny flik',
 			fullExp: "Visa kartan i fullskärm",
 			reset: "Återställ",
-			back: 'Tillbaka'
+			back: 'Tillbaka',
+			notAllowed: 'Helskärmsläge stöds eller tillåts inte av din webläsare. Funktionen kommer inte vara tillgänglig'
 		},
 		"en": {
 			viewLarge: 'Show larger map',
 			newTab: 'Open map in new tab',
 			fullExp: "Show in fullscreen",
 			reset: "Reset",
-			back: 'Go back'
+			back: 'Go back',
+			notAllowed: 'Fullscreen mode not supported/allowed by your browser. Feature will not be available.'
 		}
 	},
 	
@@ -85,7 +85,7 @@ L.Control.FullScreen = L.Control.extend({
 		this._setLang(options.langCode);
 	},
 
-	onAdd: function(map) {
+	onAdd: function(map) {  
 		this.map = map;
 		var self = this;
 		
@@ -94,32 +94,47 @@ L.Control.FullScreen = L.Control.extend({
 		
 		this.$container = $(this._container);
 
-		this._createBtn();
-
-		if (screenfull.enabled) {
-			document.addEventListener(screenfull.raw.fullscreenchange, function () {
-				self._updateButton();
-			});
+		// If mode is full, check if browser allows fullscreen mode and if map is in iframe. Else just create the button, no questions asked...
+		if (this.options.mode === 'full') {
+			if (this._enableIframeFullscreen() && screenfull.enabled) {
+				this._createBtn();
+				document.addEventListener(screenfull.raw.fullscreenchange, function () {
+					self._updateButton();
+				});
+			}
+			else {
+				console.log(self.lang.notAllowed);
+			}
 		}
-
+		else {
+			this._createBtn();
+		}
+		
 		return this._container;
-	},
+},
 
 	activate: function() {
 
 		this._properties().action();
 	},
 
-	_enableFullscreen: function() {
-		try {
-			var iframe = window.parent.document.getElementsByTagName('iframe')[0];
-			iframe.setAttribute('allowfullscreen', '');
-			utils.log(iframe);
+	_enableIframeFullscreen: function() {
+		//if not on top, then in iframe
+		if (self !== top) {
+		// Parent's not on the same domain = error.
+			try {
+				var iframe = window.parent.document.getElementsByTagName('iframe')[0];
+				iframe.setAttribute('allowfullscreen', '');
+				return true;
+			}
+			catch(error) {
+				console.log(error.message);
+				return false;
+			}
 		}
-		catch(err) {
-			utils.log(err.message);
+		else {
+			return true
 		}
-		
 	},
 
 	_updateButton: function() {
@@ -147,21 +162,15 @@ L.Control.FullScreen = L.Control.extend({
 
 	_createBtn: function() {
 		var self = this;
-
 		this.$btn = $('<button id="smap-fullscreen-btn" title="' + self._properties().expTitle + '" class="btn btn-default"><span class="' + self._properties().expand + '"></span></button>');
 		this.$btn.on("click", function () {
 			self.activate();
 			return false;
 		});
-
-		this.$container.append(this.$btn);		
+		this.$container.append(this.$btn);
 	}
 });
 
-/*
- * This code lets us skip "new" before the
- * Class name when instantiating it.
- */
  L.control.fullScreen = function (options) {
  	return new L.Control.FullScreen(options);
  };
