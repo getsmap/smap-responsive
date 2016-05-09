42b0627a617cb35b98cb4982dd38ce280819fe55
johanlahti <johanlahti@gmail.com>
2016-05-09 17:50:24 +0200

    #65
    Basic structure working ok

--- plugins/Opacity/Opacity.js
+++ plugins/Opacity/Opacity.js
@@ -8,14 +8,21 @@ L.Control.Opacity = L.Control.extend({
 		"sv": {
 			caption: "Ändra genomskinlighet på lager",
 			close: "Stäng",
-			btnTitle: "Justera transparens",
-			popoverTitle: "Titel"
+			btnTitle: "Justera genomskinlighet",
+			popoverTitle: "Genomskinlighet",
+			transparency: "Genomskinlighet",
+			transparent: "Genomskinlig",
+			opaque: "Synlig"
+
 		},
 		"en": {
 			caption: "Transparency tool",
 			close: "Close",
 			btnTitle: "Adjust transparency",
-			popoverTitle: "Title"
+			popoverTitle: "Transparency",
+			transparency: "Transparency",
+			transparent: "Transparent",
+			opaque: "Visible"
 		}
 	},
 
@@ -72,35 +79,102 @@ L.Control.Opacity = L.Control.extend({
 	_createId: function(layerId) {
 		return "opacity-"+layerId;
 	},
+	_unCreateId: function(sliderId) {
+		return sliderId.replace("opacity-", "");;
+	},
+
+
+	_setLabelValue: function($label, val) {
+		$label.text(val + " %");
+	},
+
+	_setLayerOpacity: function(layer, val) {
+		if (layer.setOpacity) {
+			layer.setOpacity( val );
+		}
+		else {
+			layer.eachLayer(function(marker) {
+				marker.setOpacity( val );
+			});
+		}
+	},
+
+	onSlide: function(e) {
+		// console.log(e);
+		var $target = $(e.target);
+		var layerId = this._unCreateId( $target.prop("id") );
+		var layer = smap.cmd.getLayer(layerId);
+		
+		this._setLayerOpacity(layer, 1-(e.value/100));
+		
+		var $valueLabel = $target.parent().find(".smap-opacity-value");
+		this._setLabelValue($valueLabel, e.value);
+
+
+	},
 
 	_addRow: function(layer) {
-		var c = this.$popContainer;
+		var $c = this.$popContainer;
 		var theId = this._createId(layer.options.layerId);
-		if ( c.find("#"+theId).length ) {
+		if ( $c.find("#"+theId).length ) {
 			return false;
 		}
-		var row = '<input type="text" id="'+theId+'" data-provide="slider" '+
-					// 'data-slider-ticks="[1, 2, 3]" '+
-					// 'data-slider-ticks-labels='["short", "medium", "long"]' '+
-					'data-slider-min="0" data-slider-max="100" data-slider-step="10" data-slider-value="'+layer.opacity+'" '+
-					'data-slider-tooltip="hide">';
-		c.append(row);
-		$('#'+theId).slider({
-			formatter: function(value) {
-				return 'Current value: ' + value;
+		if (!layer) {
+			return;
+		}
+		var opacity;
+		if (!layer.setOpacity && layer._layers) {
+			// Check if the opacity can be changed for features in this layer. If not, return.
+			var firstFeature;
+			for (var key in layer._layers) {
+				firstFeature = layer._layers[key];
+				break;
 			}
+			if (firstFeature && firstFeature.setOpacity) {
+				opacity = firstFeature.options.opacity;
+			}
+			else {
+				return;
+			}	
+		}
+		opacity = opacity || (typeof layer.options.opacity === "number" ? layer.options.opacity : 1);
+		var displayValue = (1 - opacity )*100;
+		var row = '<div class="smap-opacity-row">'+
+					'<div class="smap-opacity-values">'+
+						'<span class="smap-opacity-label">'+(layer.options.displayName || layer.options.layerId || "no name")+'</span>'+
+						'<span class="smap-opacity-value">'+displayValue+'</span>'+
+					'</div>'+
+					'<input type="text" id="'+theId+'" data-provide="slider" '+
+					'data-slider-min="0" data-slider-max="100" data-slider-step="10" data-slider-value="'+displayValue+'" '+
+					'data-slider-tooltip="hide">'+
+				'</div>';
+		$c.prepend(row);
+
+		var $slider = $c.find('#'+theId);
+		var textTransparency = this.lang.transparency;
+		$slider.slider({
+			tooltip_position: 'bottom'
+			// ticks: [0, 100],
+			// ticks_positions: [0, 100],
+			// ticks_labels: [this.lang.opaque, this.lang.transparent],
+			// formatter: function(value) {
+			// 	return value+" %";
+			// 	// return textTransparency+": "+ value+" %";
+			// }
 		});
+		$slider.on("slide", this.onSlide.bind(this));
+		this._setLabelValue($c.find(".smap-opacity-value"), displayValue);
 	},
 
 	_removeRow: function(layer) {
 		var c = this.$popContainer;
 		var theId = this._createId(layer.options.layerId);
-		c.find("#"+theId).remove();
+		c.find("#"+theId).parent().remove();
 	},
 
 	_createBtn: function() {
 		var self = this;
-		var $btn = $('<button id="smap-opacity-btn" title="' + this.lang.btntitle + '" class="btn btn-default"><span class="fa fa-adjust"></span></button>');
+		var $btn = $('<button id="smap-opacity-btn" title="' + this.lang.btnTitle + '" class="btn btn-default"><span class="fa fa-adjust"></span></button>');
 		$btn.on("click", function () {
 			self.activate();
 			return false;
@@ -113,21 +187,21 @@ L.Control.Opacity = L.Control.extend({
 			// container: 'body',
 			content: function() {
 				// console.log($(this).data("bs.popover"));
-				$(this).data("bs.popover").$tip.append( self.$popContainer );
+				$(this).data("bs.popover").$tip.find(".popover-content").append( self.$popContainer );
 			},
 			placement: "bottom",
-			title: this.lang.popoverTitle,
+			// title: this.lang.popoverTitle, // set by the btn's title attribute
 			trigger: "click"
 		});
 
 		// Draw dropdown
 		$btn.on("shown.bs.popover", function() {
-			// var $popover = $(".popover"),
-			// 	$popCont = $(".smap-opacity-popover");
+			var $popover = $(".popover");
+				// $popCont = $(".smap-opacity-popover");
 			// if (!self.options.showPopoverTitle) {
 			// 	$popover.find("h3").remove();
 			// }
-			// $popover.addClass("thandler-popover");
+			$popover.addClass("opacity-popover");
 		});
 	},
 
