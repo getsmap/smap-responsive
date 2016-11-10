777818f9110ed49f6951221abb2c207c425ffc4c
Benny Jonsson <benny.jonsson@lund.se>
2016-11-10 16:27:37 +0100

    Removed duplicat and FIR link

--- configs/lund/config.js
+++ configs/lund/config.js
@@ -1,7 +1,6 @@
 var ws = {
 	"localhost": {
-		proxy: "http://localhost:8080/proxy.py?url="
-		//proxy: "http://localhost/cgi-bin/proxy.py?url="
+		proxy: "http://kartor.lund.se/proxy.py?url="
 	},
 	"kartor.lund.se": {
 		proxy: "http://kartor.lund.se/proxy.py?url="
@@ -19,15 +18,6 @@ var config = {
 	},
 	ws: ws,
 
-	// Optional configuration object for the Leaflet map object. You can use all options specified here: http://leafletjs.com/reference.html#map-class
-	// mapConfig: {
-	// 	maxBounds: [	// Optional. Limit panning of the map. Given as [[north, west], [south, east]]
-	// 		[55.71628170645908, 12.6507568359375],
-	// 		[55.42589636057864, 13.34564208984375]
-	// 	],	
-	// 	minZoom: 11,	// Optional. Limit how much you can zoom out. 0 is maximum zoomed out.
-	// 	maxZoom: 18 	// Optional. Limit how much you can zoom in. 18 is usually the maximum zoom.
-	// },
 
 	smapOptions: {
 		// The text of the <title>-tag
@@ -52,18 +42,7 @@ var config = {
 				maxZoom: 18
 			}
 		},
-		// {
-		// 	init: "L.BingLayer",
-		// 	//url: 'http://api.tiles.mapbox.com/v4/lundskommun.j909n073/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibHVuZHNrb21tdW4iLCJhIjoiTGRtQW51WSJ9.f-bABPBDFFzgUc3UkBsAGA#12/55.6922/13.2732',
-		// 	key: "ArnlcFILNVTLn5NnwH731HwoKcUDS5hSbTTMq5U0Cd5jYwv7zvUPWgCJvT99krNa",
-		// 	options: {
-		// 		layerId: "binglayer",
-		// 		displayName: "Flygfoto, Bing aerial",
-		// 		category: ["Flygfoto"],
-		// 		attribution: '<a href="http://www.microsoft.com/maps/assets/docs/terms.aspx" target="_blank">Bing maps TOU</a>',
-		// 		maxZoom: 18
-		// 	}
-		// },
+
 		{
 			init: "L.TileLayer.WMS",
 			url: "http://kartor.lund.se/geoserver/wms?",
@@ -84,25 +63,6 @@ var config = {
 
 	// -- Overlays --
 	ol: [
-		// {
-		// init: "L.TileLayer.WMS",
-		// url: "http://kartor.lund.se/geoserver/wms",
-		// options: {
-		// // legend: false,
-		// //category: ["Uppleva & göra","Bibliotek"],
-		// layerId: "lkarta_kommungrans",
-		// displayName: "Kommungräns",
-		// layers: 'lkarta_kommungrans',
-		// format: 'image/png',
-		// featureType: "polygon",
-		// selectable: false,
-		// transparent: true,
-		// opacity: 1,
-		// attribution: "@ Lunds kommun",
-		// //popup: "<p>${namn_verks} <br/> ${adress}</p><p>${beskr}</p><p><a href=${url}>läs mer</a></p>",
-		// zIndex: 10
-		// }
-		// },
 		{
 			init: "L.TileLayer.WMS",
 			url: "http://kartor.lund.se/geoserver/wms",
@@ -180,32 +140,12 @@ var config = {
 				//zIndex: 9
 			}
 		},
-		//  {
-		//     init: "L.TileLayer.WMS",
-		// 	url: "http://193.17.67.229/geoserver/common/wms?",
-		//     parentTag: "service",
-		//     options: {
-		//         //legend: true,
-		//         category: ["Trafik & infrastruktur"],
-		//         layerId: "hallplatser",
-		//         displayName: "Hållplatser",
-		//         layers: 'common:lund_hallplatser',
-		//         format: 'image/png',
-		// 		info_format: 'text/json',
-		//         selectable: true,
-		//         transparent: true,
-		//         opacity: 1,
-		//         attribution: "@ Skånetrafiken	",
-		//         popup: "<p>${caption}</p>",
-		//         zIndex: 10
-		//     }
-		//  },
 
 		{
 			init: "L.esri.dynamicMapLayer",
 			url: "http://arcgisserver.lund.se/arcgis/rest/services/extern/Registerkarta_Lund/MapServer",
 			options: {
-				legend: "img/legend/fastinfo.png",
+			//	legend: "img/legend/fastinfo.png",
 				category: ["Bygga, bo & miljö"],
 				displayName: "Fastigheter",
 				transparent: true,
@@ -232,7 +172,7 @@ var config = {
 				transparent: true,
 				opacity: 1,
 				attribution: "@ Lunds kommun",
-				popup: "<p>${fastighet}</p><p><a href=${url1}>FIR</a></p>",
+				popup: "<p>${fastighet}</p>",
 				zIndex: 9
 			}
 		},
@@ -255,30 +195,8 @@ var config = {
 				popup: "<p>${extid}</p><p><a href=${url}>länk till plan</a></p>",
 				zIndex: 9
 			}
-		},	
-		{
-                	init: "L.TileLayer.WMS",
-                	url: "http://kartor.lund.se/geoserver/wms",
-                	options: 
-			{
-                    		//legend: true,
-                    	    category: ["Bygga, bo & miljö"],
-                    		layerId: "plan_pagaende",
-                    		displayName: "Pågående planer",
-                    		layers: 'plan_pagaende',
-                    		format: 'image/png',
-                    		featureType: "polygon",
-                    		selectable: true,
-                    		transparent: true,
-                    		opacity: 1,
-                    		attribution: "@ Lunds kommuns",
-                    		popup: "<p>Plan: ${popularnamn}</p><p>Nummer: ${pa}</p><p>Skede: ${status}</p><p><a href=http://www.lund.se/${pa}>läs mer</a></p>",
-                    		zIndex: 10,
-				
-           		}
-
-
 		},
+	
 		{
 			init: "L.TileLayer.WMS",
 			url: "http://kartor.lund.se/geoserver/wms",
@@ -321,9 +239,6 @@ var config = {
 				zIndex: 9
 			}
 		},
-
-
-
 		{
 			init: "L.TileLayer.WMS",
 			url: "http://kartor.lund.se/geoserver/wms",
@@ -343,7 +258,6 @@ var config = {
 				zIndex: 9
 			}
 		},
-
 		{
 			init: "L.TileLayer.WMS",
 			url: "http://kartor.lund.se/geoserver/wms",
@@ -383,8 +297,6 @@ var config = {
 				//zIndex: 9
 			}
 		},
-
-
 		{
 			init: "L.TileLayer.WMS",
 			url: "http://kartor.lund.se/geoserver/wms",
@@ -480,67 +392,7 @@ var config = {
 				popup: "<p>${namn_verks}</p><p>${beskr}</p><p><a href=${url}>hemsida</a></p>",
 				zIndex: 10
 			}
-		},
-		/* {
-			 init: "L.TileLayer.WMS",
-			 url: "http://kartor.lund.se/geoserver/wms",
-			 
-			 options: {
-				 //legend: true,
-				 category: ["Uppleva & göra","Idrott & Fritid"],
-				 layerId: "lkarta_idrott_fritid_faltgruppen",
-				 displayName: "Fältgruppen",
-				 layers: 'lkarta_idrott_fritid_faltgruppen',
-				 format: 'image/png',
-				 featureType: "polygon",
-				 selectable: true,
-				 transparent: true,
-				 opacity: 1,
-				 attribution: "@ Lunds kommun",
-				 popup: "<p><a href=${url}>hemsida</a></p>",
-				 zIndex: 10
-			 }
-		 },
-				{
-			 init: "L.TileLayer.WMS",
-			 url: "http://kartor.lund.se/geoserver/wms",
-			 
-			 options: {
-				 //legend: true,
-				 category: ["Uppleva & göra","Idrott & Fritid"],
-				 layerId: "lkarta_idrott_fritid_ungpolitik",
-				 displayName: "Ungdomspolitik",
-				 layers: 'lkarta_idrott_fritid_ungpolitik',
-				 format: 'image/png',
-				 featureType: "polygon",
-				 selectable: true,
-				 transparent: true,
-				 opacity: 1,
-				 attribution: "@ Lunds kommun",
-				 popup: "<p>${namn_verks}</p><p>${beskr}</p><p><a href=${url}>hemsida</a></p>",
-				 zIndex: 10
-			 }
-		 }, 
-		{
-			 init: "L.TileLayer.WMS",
-			 url: "http://kartor.lund.se/geoserver/wms",
-			 
-			 options: {
-				 //legend: true,
-				 category: ["Uppleva & göra","Idrott & Fritid"],
-				 layerId: "lkarta_idrott_fritid_aventyr",
-				 displayName: "Äventyrspedagogik",
-				 layers: 'lkarta_idrott_fritid_aventyr',
-				 format: 'image/png',
-				 featureType: "polygon",
-				 selectable: true,
-				 transparent: true,
-				 opacity: 1,
-				 attribution: "@ Lunds kommun",
-				 popup: "<p>${beskr}</p><p><a href=${url}>hemsida</a></p>",
-				 zIndex: 10
-			 }
-		 },*/
+		},	
 		{
 			init: "L.TileLayer.WMS",
 			url: "http://kartor.lund.se/geoserver/wms",
@@ -601,10 +453,6 @@ var config = {
 				zIndex: 110,
 			}
 		},
-
-
-
-		//
 
 		{
 			init: "L.TileLayer.WMS",
@@ -780,29 +628,6 @@ var config = {
 			}
 		}
 
-		//------------------------------------TOALETTER---------------------------------------------------------------		
-		//,
-		// {
-		// init: "L.TileLayer.WMS",
-		// url: "http://kartor.lund.se/geoserver/wms",
-		// parentTag: "service",
-		// options: {
-		// //legend: true,
-		// category: ["Trafik & Service"],
-		// layerId: "toaletterTM",
-		// displayName: "Toaletter",
-		// layers: 'lkarta_toaletterTM',
-		// format: 'image/png',
-		// featureType: "polygon",
-		// selectable: true,
-		// transparent: true,
-		// opacity: 1,
-		// attribution: "@ Lunds kommun",
-		// popup: "<p>${namn}</p><p>${oppetdagar}</p><p>${oppettider}</p><p>${oppetolik}</p>",
-		// zIndex: 10
-		// }
-		// }
-
 	],
 
 	// <><><><><><><><><><><><><><><><><><><><><><><><><><>
@@ -814,74 +639,7 @@ var config = {
 	// <><><><><><><><><><><><><><><><><><><><><><><><><><>
 
 	plugins: [
-		// <><><><><><><><><><><><><><><><><><><><><><><><><><>
-		// Lets the user adjust the opacity of most layers added 
-		// to the map.
-		// <><><><><><><><><><><><><><><><><><><><><><><><><><>
-		// {
-		// 	init: "L.Control.Opacity",
-		// 	options: {
-		// 		position: 'topright',	// Leaflet container (default is topright)
-		// 		showTitle: false,		// Show the popover title
-		// 		btnReset: true			// Show a reset button and a "zero" button
-		// 	}
-		// },
-		// <><><><><><><><><><><><><><><><><><><><><><><><><><>
-		// Creates a button e.g. in the toolbar which simply redirects
-		// the user to another URL. You can create several version of this
-		// plugin for the same map.
-		// <><><><><><><><><><><><><><><><><><><><><><><><><><>
-		// {
-		// 	init: "L.Control.Redirect",
-		// 	options: {
-		// 		position: 'topright',		// Leaflet container (default is topright)
-		// 		url: '//malmo.se/kartor',	// Where to redirect
-		// 		target: 'newTab',			// How to open the link: sameWindow or newTab
-		// 		btnClass: "fa fa-home",		// The icon class (e.g. fa or glyphicon)
-		// 		_lang: {						
-		// 			sv: {
-		// 				name: "Länk till Malmös kartor"			// Optional custom tooltip
-		// 			},
-		// 			en: {
-		// 				name: "Link to Malmö's maps"			// Optional custom tooltip
-		// 			}
-		// 		}
-		// 	}
-		// },
-		// <><><><><><><><><><><><><><><><><><><><><><><><><><>
-		// RedirectClick opens a new browser tab when the user clicks on the map.
-		// The easting ${x} and northing ${y} is sent along to the url. See example below.
-		// You can create several version of this plugin for the same map.
-		// <><><><><><><><><><><><><><><><><><><><><><><><><><>
-		// {
-		// 	init : "L.Control.RedirectClick", // Pictometry
-		// 	options: {
-		// 		position: "topright",		// Button's position
-		// 		url: "http://www.malmo.se/karta?xy=${x},${y},This%20is%20where%20you%20clicked", 	// Where to get linked to (including coordinates)
-		// 		btnClass: "fa fa-plane",	// Button's icon class
-		// 		cursor: "crosshair",		// Cursor shown in map before click
-		// 		destProj: "EPSG:4326",		// Optional. Convert the clicked coordinates to this coordinate system before inserting into URL
-		// 		reverseAxisDest: false,		// Optional. Some projections have inverted the x and y axis (applies to destination projection)
-		// 		_lang: {
-		// 			en: {
-		// 				name: "Click the map to be redirected" // tooltip for the button in English
-		// 			},
-		// 			sv: {
-		// 				name: "Klicka i kartan och länkas vidare" // tooltip for the button in Swedish
-		// 			}
-		// 		}
-		// 	}
-		// },
-		// <><><><><><><><><><><><><><><><><><><><><><><><><><>
-		// Opacity lets the user change the opacity of currently visible layers.
-		// <><><><><><><><><><><><><><><><><><><><><><><><><><>
-
-		// {
-		// 	init: "L.Control.Opacity",
-		// 	options: {
-		// 		addToMenu: true
-		// 	}
-		// },
+	
 		// <><><><><><><><><><><><><><><><><><><><><><><><><><>
 		// Scale is Leaflet's in-built scale bar control. See options: http://leafletjs.com/reference.html#control-scale
 		// <><><><><><><><><><><><><><><><><><><><><><><><><><>
@@ -964,57 +722,17 @@ var config = {
 		},
 
 		{
-			init: "L.Control.SearchLund",
+			init: "L.Control.LundSearch",
 			options: {
 				gui: true,
 				useProxy: true,
 				wsAcUrl: "http://kartor.lund.se/gist/objectsearch",
 				wsLocateUrl: "http://kartor.lund.se/gist/getobject",
-				//wsAcUrl : "http://kartor.lund.se/gist/multisearch",
-				//wsLocateUrl: "http://kartor.lund.se/gist/multisearch",
-				//wsAcUrl : "http://kartor.lund.se/lkarta_sokproxy/auto_lund.ashx",
-				//wsLocateUrl: "http://kartor.lund.se/lkarta_sokproxy/sokexakt_lund.ashx",
-				//wsAcUrl : "http://nominatim.openstreetmap.org/search",
-				//wsLocateUrl : "http://nominatim.openstreetmap.org/search",
-				wsOrgProj: "EPSG:4326"
+				wsOrgProj: "EPSG:4326",
 			}
 		},
 
-		// <><><><><><><><><><><><><><><><><><><><><><><><><><>
-		// Search connects to an autocomplete and geolocate service and places a marker
-		// at the geolocated location.
-		// <><><><><><><><><><><><><><><><><><><><><><><><><><>
-		// {
-		// 	init: "L.Control.Search",
-		// 	options: {
-		// 		_lang: {
-		// 			// Watermark/placeholder for text entry. Language dependent.
-		// 			"en": { search: "Search address or place" },	// english
-		// 			"sv": { search: "Sök adress eller plats" }	// swedish
-		// 		},
-		// 		wsAcUrl: "//kartor.malmo.se/api/v1/addresses/autocomplete/", // Required. Autocomplete service.
-		// 		wsLocateUrl: "//kartor.malmo.se/api/v1/addresses/geolocate/", // Required. Geolocate service.
-		// 		gui: true,					// If false, entry is not shown but the plugin can still take the POI URL parameter
-		// 		whitespace: "%20",			// How to encode whitespace.
-		// 		wsOrgProj: "EPSG:3008",		// The projection of the returned coordinates from the web service
-		// 		useProxy: false,			// If you want call the URL with a prepended proxy URL (defined in ws above)
-		// 		acOptions: {				// typeahead options (Bootstrap's autocomplete library)
-		// 			items: 100				// Number of options to display on autocomplete
-		// 		}
-		// 	}
-		// },
-
-		// <><><><><><><><><><><><><><><><><><><><><><><><><><>
-		// Print creates a downloadable image server-side. Requires Geoserver and the plugin "Mapfish print".
-		// <><><><><><><><><><><><><><><><><><><><><><><><><><>
-		// {
-		// 		init: "L.Control.Print",
-		// 		options: {
-		// 			printUrl: "//kartor.malmo.se/print-servlet/leaflet_print/",		// The print service URL
-		// 			position: "topright"											// Button's position
-		// 		}
-		// 	},
-
+	
 		// <><><><><><><><><><><><><><><><><><><><><><><><><><>
 		// ShareLink adds a button which, on click, will create a
 		// URL which recreates the map, more or less how it looked like.
@@ -1039,70 +757,7 @@ var config = {
 			}
 		},
 
-		// <><><><><><><><><><><><><><><><><><><><><><><><><><>
-		// Info simply creates a toggleable Bootstrap modal which you can fill with any info below.
-		// <><><><><><><><><><><><><><><><><><><><><><><><><><>
-		// {
-		// 	init: "L.Control.Info",
-		// 	options: {
-		// 		addToMenu: true,				// Creates a button that toggles the info dialog (if autoActivate is false this should probably be set to true)
-		// 		autoActivate: true,				// If you want the Info dialog to open from start, set this to true
-		// 		dontShowAgainBox: true,			// Requires autoActivate: true. Allows the user to check the box to not show this box again (using localStorage)
-		// 		dateLastUpdated: "2016-03-31",	// Requires dontShowAgainBox: true. Setting a date here means dontShowAgain checkbox checked by user before this date will expire.
-		// 		daysExpired: 90,				// Requires dontShowAgainBox: true. If set, the dontShowAgain choice will expire after this many days.
-		// 		position: "topright",			// Requires addToMenu: true. Button's position.
-
-		// 		// Here follows the content of the modal – language dependent!
-		// 		_lang: {
-		// 			"en": {
-		// 				titleInfo: "<h4>A test header</h4>",
-		// 				bodyContent:
-		// 					'<p>Some test content</p>'
-		// 			},
-		// 			"sv": {
-		// 				titleInfo: "<h4>En testrubrik</h4>",
-		// 				bodyContent:
-		// 					'<p>Lite testinnehåll</p>'
-		// 			}
-		// 		}
-		// 	}
-		// },
-
-		// <><><><><><><><><><><><><><><><><><><><><><><><><><>
-		// MeasureDraw is a combined measure and drawing tool. The created
-		// markers, lines or polygons can be shared with others 
-		// (geometries and attributes sent along as a URL parameter).
-		// <><><><><><><><><><><><><><><><><><><><><><><><><><>
-		// {
-		// 	init: "L.Control.MeasureDraw",
-		// 	options: {
-		// 		position: "topright",		// Button's position
-		// 		saveMode: "url",			// So far url is the only option
-		// 		layerName: "measurelayer",	// The internal layerId for the draw layer
-
-		// 		stylePolygon: {				// Draw style for polygons
-		// 			color: '#0077e2',
-		// 			weight: 3
-		// 		},
-		// 		stylePolyline: {			// Draw style for polylines
-		// 			color: '#0077e2',
-		// 			weight: 9
-		// 		}
-		// 	}
-		// },
-
-		// <><><><><><><><><><><><><><><><><><><><><><><><><><>
-		// ToolHandler takes care of making all buttons inside the top-right div responsive.
-		// When the screen width is smaller than the defined breakpoint, the buttons are contained 
-		// within a Bootstrap popover which can be toggled by a single button.
-		// <><><><><><><><><><><><><><><><><><><><><><><><><><>
-		// {
-		// 	init: "L.Control.ToolHandler",
-		// 	options: {
-		// 		showPopoverTitle: false 	// Show title (header) in the popover
-		// 	}
-		// }
-		// ,
+		
 		{
 			init: "L.Control.HistoryBack",
 			options: {
@@ -1122,16 +777,6 @@ var config = {
 
 		},
 
-
-		// <><><><><><><><><><><><><><><><><><><><><><><><><><>
-		// Add2HomeScreen creates a popover on iOS devices supporting
-		// "Add To Homescreen", which advices the user to add the website
-		// to the homescreen, making it look almost like a native app.
-		// <><><><><><><><><><><><><><><><><><><><><><><><><><>
-		// {
-		//	init: "L.Control.Add2HomeScreen",
-		//	options: {}
-		// }
 		{
 			init: "L.Control.LundHeader",
 			options: {
